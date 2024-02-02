a95e2a555edf5cff7107beba99f3a6601888fd11
kzhsw <120431159+kzhsw@users.noreply.github.com>
2024-02-02 14:32:11 +0800

    Update S3EscaperBenchmark.java

--- src/main/java/kzhsw/S3EscaperBenchmark.java
+++ src/main/java/kzhsw/S3EscaperBenchmark.java
@@ -7,7 +7,23 @@ import java.util.concurrent.TimeUnit;
 @State(Scope.Thread)
 public class S3EscaperBenchmark {
 
-    @Param({"my-bucket", "photos/2024/02/02", "file.txt?versionId=3L137"})
+    @Param({
+        "my-bucket",
+        "photos/2024/02/02",
+        "file.txt?versionId=3L137",
+        "user@domain.com",
+        "document.pdf",
+        "music/2024/album/song.mp3",
+        "video/2024/movie.mp4?versionId=1A2B",
+        "data/archive.zip",
+        "application/database.sqlite",
+        "folder/subfolder/file.txt",
+        "special-characters/!$&'()*+,/:;=@[]",
+        "emoji/😀😃😄😁😆😅😂🤣",
+        "non-english/こんにちは/안녕하세요",
+        "long-string/" + "a".repeat(1000),
+        "deep-nesting/" + "folder/".repeat(100) + "file"
+    })
     public String testString;
 
     @Benchmark
